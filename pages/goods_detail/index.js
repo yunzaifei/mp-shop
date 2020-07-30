d80f7682392328ef11e7f622f1345602874a1b2e
yunzaifei <zyunfei86@hotmail.com>
2020-07-30 21:01:43 +0800

    商品详情页

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,47 @@
 // pages/goods_detail/index.js
+import {request} from '../../service/index';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const { goods_id } = options;
+    this.getGoodDetail(goods_id);
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取商品详情
    */
-  onReady: function () {
-
+  getGoodDetail(goods_id) {
+    request({
+      url: '/goods/detail',
+      data: { goods_id }
+    }).then(res => {
+      const { goods_name, goods_price, goods_introduce, pics } = res;
+      this.setData({
+        goodsObj: {
+          goods_name,
+          goods_price,
+          goods_introduce: goods_introduce.replace('.webp', '.jpg'),
+          pics,
+        }
+      });
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handlePreviewImage(e) {
+    console.log('*****************');
+    const urls = this.data.goodsObj.pics.map(v => v.pics_mid);
+    const {url} = e.currentTarget.dataset;
+    wx.previewImage({
+      urls,
+      current: url,
+    })
   }
 })
