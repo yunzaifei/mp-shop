2dfdf4d05a6f2bc8efeaeef7a844908c546af660
yunzaifei <zyunfei86@hotmail.com>
2020-08-04 15:06:54 +0800

    购物车页面

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: {},
+    cartList: []
   },
 
   /**
@@ -14,53 +15,41 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    const address = wx.getStorageSync('address');
+    const cartList = wx.getStorageSync('cart');
+    this.setData({ 
+      address, 
+      cartList,
+    });
   },
-
   /**
-   * 生命周期函数--监听页面隐藏
+   * 点击收货地址
    */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleChooseAddress() {
+    wx.getSetting({
+      withSubscriptions: true,
+      success: (result) => {
+        const scopeAddress = result.authSetting["scope.address"];
+        const successAddress = (res) => {
+          console.log(res);
+          res.addressAll = res.provinceName+res.cityName+res.countyName+res.detailInfo;
+          wx.setStorageSync('address', res);
+        };
+        if(scopeAddress === false) {
+          wx.openSetting({
+            withSubscriptions: true,
+            success: () => {
+              wx.chooseAddress({ success: successAddress });
+            }
+          })
+        } else {
+          wx.chooseAddress({ success: successAddress });
+        }
+      }
+    })
   }
 })
